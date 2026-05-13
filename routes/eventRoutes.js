f3f32152d16a3bd5b464c841fb444246d3bffce9
Masilo Pudikabekwa <603037@student.belgiumcampus.ac.za>
2026-05-13 20:49:44 +0200

    Admins can now create events

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -5,6 +5,13 @@ const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
 
 router.get('/', eventController.getAllEvents);
 router.get('/manage', isAuthenticated, isAdmin, eventController.getManagePage);
+
+router.get('/create', isAuthenticated, isAdmin, (req, res) => {
+    res.render('create-event', {
+      title: 'Create Event'
+    });
+});
+
 router.post('/create', isAuthenticated, isAdmin, eventController.createEvent);
 router.get('/edit/:id', isAuthenticated, isAdmin, eventController.getEditEvent);
 router.put('/edit/:id', isAuthenticated, isAdmin, eventController.updateEvent);
